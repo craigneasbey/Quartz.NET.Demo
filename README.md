c0f934ef5f47ed43a90caf36454d7ef3003c1866
Craig Neasbey <craigneasbey@users.noreply.github.com>
2021-11-20 15:28:28 +1100

    Base Quartz.NET ASP.NET Core Web Project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Quartz.NET.Demo
+h1. Quartz.NET.Demo
+
+h2. Development
+dotnet new web
+dotnet add ./Quartz.NET.Demo.csproj package Quartz.AspNetCore -v 3.3.3 -s https://api.nuget.org/v3/index.json
+dotnet add ./Quartz.NET.Demo.csproj package Quartz.Extensions.DependencyInjection -v 3.3.3 -s https://api.nuget.org/v3/index.json
---
+++ Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Quartz.NET.Demo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            // base configuration from appsettings.json
+            services.Configure<QuartzOptions>(Configuration.GetSection("Quartz"));
+
+            services.AddQuartz(q =>
+            {
+                // handy when part of cluster or you want to otherwise identify multiple schedulers
+                q.SchedulerId = "Scheduler-Core";
+
+                q.UseMicrosoftDependencyInjectionJobFactory();
+
+                // these are the defaults
+                q.UseSimpleTypeLoader();
+                q.UseInMemoryStore();
+                q.UseDefaultThreadPool(tp =>
+                {
+                    tp.MaxConcurrency = 10;
+                });
+
+                // base quartz scheduler, job and trigger configuration
+            });
+
+            // ASP.NET Core hosting
+            services.AddQuartzServer(options =>
+            {
+                // when shutting down we want jobs to complete gracefully
+                options.WaitForJobsToComplete = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
